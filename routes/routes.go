72bf9a707fa573e41553aebce9b8ebd152ea547c
brookeluo <brooke.luopeng@gmail.com>
2024-06-02 23:14:56 +0800

    add path

--- routes/routes.go
+++ routes/routes.go
@@ -9,22 +9,29 @@ func Router() *gin.Engine {
 
 	r := gin.Default()
 
-	// 用户相关的路由组
-	userGroup := r.Group("/user")
+	// 创建一个带有前缀的路由组 /api/v2
+	apiV2 := r.Group("/api/v2")
 	{
-		userGroup.POST("/login", controllers.UserLoginService)
-		userGroup.POST("/register", controllers.UserRegisterService)
-		userGroup.GET("/userinfo", controllers.UserInfoService)
+		// 用户相关的路由组
+		userGroup := apiV2.Group("/user")
+		{
+			userGroup.POST("/login", controllers.UserLoginService)
+			userGroup.POST("/register", controllers.UserRegisterService)
+			userGroup.GET("/userinfo", controllers.UserInfoService)
+		}
+
+		// 任务相关的路由组
+		taskGroup := apiV2.Group("/task")
+		{
+			taskGroup.POST("/list", controllers.TaskListService)
+			taskGroup.POST("/add", controllers.TaskAddService)
+			taskGroup.POST("/edit", controllers.TaskEdit)
+			taskGroup.DELETE("/delete", controllers.TaskDelete)
+		}
 	}
 
-	// 任务相关的路由组
-	taskGroup := r.Group("/task")
-	{
-		taskGroup.POST("/list", controllers.TaskListService)
-		taskGroup.POST("/add", controllers.TaskAddService)
-		taskGroup.POST("/edit", controllers.TaskEdit)
-		taskGroup.DELETE("/delete", controllers.TaskDelete)
-	}
+	// 启动服务器
+	r.Run(":8080") // 默认监听并服务于 0.0.0.0:8080
 
 	return r
 }
